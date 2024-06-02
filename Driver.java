fac1a652248b1f29c0356e21aeb974b1ab1c9f72
ndhfrayyan <74947254+ndhfrayyan@users.noreply.github.com>
2024-06-02 15:28:46 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,36 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+
+
 public class Driver {
 public static void main(String [] args) {
-	Polynomial p = new Polynomial();
-	System.out.println(p.evaluate(3));
-	double [] c1 = {6,0,0,5};
-	Polynomial p1 = new Polynomial(c1);
-	double [] c2 = {0,-2,0,0,-9};
-	Polynomial p2 = new Polynomial(c2);
-	Polynomial s = p1.add(p2);
-	System.out.println("s(0.1) = " + s.evaluate(0.1));
-	if(s.hasRoot(1))
-		System.out.println("1 is a root of s");
-	else
-		System.out.println("1 is not a root of s");
+		// test constructor
+		Polynomial P1 = new Polynomial();
+		System.out.println(P1.pc.length == 0);
+		double f[] ={-1.0, -2.0, 3.0, 4.0, 5.0};
+		int g[] = {5, 2, 4, 3, 1};
+		Polynomial P2 = new Polynomial(f, g);
+		double h[] ={-5.0, -4.0, -3.0, 2.0, 1.0}; 
+		int i[] = {1, 3, 4, 2, 5};
+		Polynomial P3 = new Polynomial(h, i);
+		// test add
+		Polynomial P5 = P2.add(P3);
+		System.out.println(P5.pc.length == 0);
+		// test multiply
+		Polynomial P4 = new Polynomial(h, g);
+		Polynomial P6 = P4.multiply(P3);
+		for(int j = 0; j < P6.pc.length; j++){
+			System.out.println(P6.pc[j] + "x" + P6.pw[j]);
+		}
+		// test evaluate
+		System.out.println(P6.evaluate(1));
+		// test hasRoot
+		System.out.println(P6.hasRoot(0));
+		// test saveToFile
+		String s = "test.txt";
+		P6.saveToFile(s);
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,39 +1,164 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+
 public class Polynomial{
 	double[] pc;
+	int[] pw;
 	public Polynomial(){
-		pc = new double[1];
-		pc[0] = 0;
+		pc = new double[0];
+		pw = new int[0];
 	}
-	public Polynomial(double[] dd){
-		pc = new double[dd.length];
-		for(int i = 0; i < dd.length; i++){
+	public Polynomial(double[] dd, int[] bb){
+		int len = dd.length;
+		pc = new double[len];
+		pw = new int[len];
+		for(int i = 0; i < len; i++){
 			pc[i] = dd[i];
+			pw[i] = bb[i];
+		}
+	}
+	public Polynomial(File f){
+		String p = f.getAbsolutePath();
+		String s = "";
+		try{
+			s = new String(Files.readAllBytes(Paths.get(p)));
+		}
+		catch(IOException e){
+            e.printStackTrace();
+        }
+		String sp[] = s.split("[+-]");
+		int len = sp.length;
+		pc = new double[len];
+		pw = new int[len];
+		for(int i = 0; i < len; i++){
+			int idx = sp[i].indexOf('x');
+			if(idx == -1){
+				pc[i] = Double.parseDouble(sp[i]);
+				pw[i] = 0;
+			}
+			else{
+				pc[i] = Double.parseDouble(sp[i].substring(0, idx - 1));
+				pw[i] = Integer.parseInt(sp[i].substring(idx + 1));
+			}
+		}
+		if(s.charAt(0) == '-') pc[0] *= -1;
+		int j = 1;
+		for(int i = 1; i < len; i++){
+			if(s.charAt(i) == '-'){
+				pc[j] *= -1;
+				j++;
+			}
+			else if(s.charAt(i) == '+'){
+				j++;
+			}
 		}
 	}
 	Polynomial add(Polynomial p){
-		int len = pc.length;
-		if(p.pc.length > len) len = p.pc.length;
-		double [] pp = new double[len];
-		for(int i = 0; i < len; i++){
-			pp[i] = 0;
-			if(i < p.pc.length) 
-				pp[i] += p.pc[i];
-			if(i < pc.length) 
-				pp[i] += pc[i];
+		int len1 = pc.length, len2 = p.pc.length;
+		double[] pp = new double[len1 + len2];
+		int[] ww = new int[len1 + len2];
+		for(int i = 0; i < len1 + len2; i++) pp[i] = ww[i] = -1;
+		for(int i = 0; i < len1 + len2; i++){
+			if(i < len1){
+				for(int j = 0; j < len1 + len2; j++){
+					if(ww[j] == -1){
+						ww[j] = pw[i];
+						pp[j] = pc[i];
+						break;
+					}
+					else if(ww[j] == pw[i]){
+						pp[j] += pc[i];
+						break;
+					}
+				}
+			}
+			else{
+				int k = i - len1;
+				for(int j = 0; j < len1 + len2; j++){
+					if(ww[j] == -1){
+						ww[j] = p.pw[k];
+						pp[j] = p.pc[k];
+						break;
+					}
+					else if(ww[j] == p.pw[k]){
+						pp[j] += p.pc[k];
+						break;
+					}
+				}
+			}
 		}
-		Polynomial ret = new Polynomial(pp);
+		int cnt = 0;
+		for(int i = 0; i < len1 + len2; i++){
+			if(ww[i] == -1) break;
+			if(pp[i] != 0) cnt++;
+		}
+		double[] rc = new double[cnt];
+		int[] cw = new int[cnt];
+		int idx = 0;
+		for(int i = 0; i < len1 + len2; i++){
+			if(ww[i] == -1) break;
+			if(pp[i] != 0){
+				rc[idx] = pp[i];
+				cw[idx] = ww[i];
+				idx++;
+			}
+		}
+		Polynomial ret = new Polynomial(rc, cw);
 		return ret;
 	}
 	double evaluate(double d){
-		double ans = 0, sub = 1;
-		for(int i = 0; i < pc.length; i++){
+		double ans = 0, sub;
+		for(int i = 0; i < pw.length; i++){
+			sub = 1;
+			for(int j = 1; j < pw[i]; j++){
+				sub *= d;
+			}
 			ans += pc[i] * sub;
-			sub *= d;
 		}
 		return ans;
 	}
 	boolean hasRoot(double d){
-		if(evaluate(d) == 0) return true;
-		return false;
+		return evaluate(d) == 0;
+	}
+	Polynomial multiply(Polynomial p){
+		int len1 = pc.length, len2 = p.pc.length;
+		Polynomial ret = new Polynomial();
+		for(int i = 0; i < len1; i++){
+			double[] pp = new double[len2];
+			int[] ww =  new int[len2];
+			for(int j = 0; j < len2; j++){
+				pp[j] = pc[i] * p.pc[j];
+				ww[j] = pw[i] + p.pw[j];
+			}
+			Polynomial added = new Polynomial(pp, ww);
+			ret = ret.add(added);
+		}
+		return ret;
+	}
+	void saveToFile(String s){
+		String o = "";
+		for(int i = 0; i < pc.length; i++){
+			if(i != 0 && pc[i] > 0){
+				o += "+";
+			}
+			o += Double.toString(pc[i]);
+			if(pw[i] != 0){
+				o += "x";
+				o += Integer.toString(pw[i]);
+			}
+		}
+		Path cur = Paths.get(s);
+		String ss = cur.toAbsolutePath().toString();
+		Path p = Paths.get(ss);
+		try{
+			Files.writeString(p, o, StandardCharsets.UTF_8);
+		}
+		catch(IOException e){
+            e.printStackTrace();
+        }
 	}
 }
